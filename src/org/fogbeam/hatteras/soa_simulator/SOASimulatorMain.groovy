4b0967c6100a56371616b2f20da8bd96c6463a1b
Phillip Rhodes <motley.crue.fan@gmail.com>
2017-05-13 01:58:28 -0400

    latest changes

--- src/org/fogbeam/hatteras/soa_simulator/SOASimulatorMain.groovy
+++ src/org/fogbeam/hatteras/soa_simulator/SOASimulatorMain.groovy
@@ -8,13 +8,14 @@ import javax.jms.Session
 
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate
 
 class SOASimulatorMain
 {	
 	public static void main( String[] args ) throws Exception
 	{	
-		ApplicationContext appContext = new ClassPathXmlApplicationContext( "senderContext.xml" );
+		ApplicationContext appContext = new FileSystemXmlApplicationContext( "conf/senderContext.xml" )
 	
 		JmsTemplate jmsTemplate = appContext.getBean( "jmsTemplate", JmsTemplate.class );
 		
@@ -23,16 +24,26 @@ class SOASimulatorMain
 		SimulatorProcessingThread simulator = new SimulatorProcessingThread();
 		simulator.jmsTemplate = jmsTemplate;
 		
-		Thread simThread = new Thread( simulator );
+		Random random = new Random();
+
+		while( true )
+		{
+			Thread simThread = new Thread( simulator );
 		
-		simThread.start();
+			simThread.start();
 		
-		Thread.sleep( 240000 );
+			Thread.sleep( 240000 );
 		
-		simulator.stopFlag = true;
-		simThread.interrupt();
-		
-				
+			simulator.stopFlag = true;
+			simThread.interrupt();
+
+			// random pause 
+			int pause = random.nextInt(45)+15;
+			long pauseMillis = pause*60*1000;
+			Thread.sleep(pauseMillis);
+					
+		}
+			
 		System.out.println( "done" );
 	}
 }
